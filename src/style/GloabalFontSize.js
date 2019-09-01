c62f49c1e755ad0d1410e4254d391f134f7d91c6
pess <sharpmsil@gmail.com>
2019-09-01 21:11:56 +0300

    Added gloabal font silze

---
+++ src/style/GloabalFontSize.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native'
+
+export const GlobalFontSilze = StyleSheet.create({
+    h1: {
+        fontSize: 32
+    },
+    h2: {
+        fontSize: 30
+    },
+    h3: {
+        fontSize: 28
+    },
+    h4: {
+        fontSize: 26
+    },
+    h5: {
+        fontSize: 24
+    },
+    h6: {
+        fontSize: 20
+    },
+    h7: {
+        fontSize: 18
+    },
+    h8: {
+        fontSize: 16
+    },
+    h9: {
+        fontSize: 14
+    },
+    h10: {
+        fontSize: 12
+    }
+})
