166d4025e64cd375208f295eceb0a5115d494f76
apastuhov <apastuhov@mmtr.ru>
2019-12-06 08:45:48 +0300

    Remove loader from start screen

--- src/components/full-screen-logo/FullScreenLogo.js
+++ src/components/full-screen-logo/FullScreenLogo.js
@@ -10,15 +10,6 @@ export class FullScreenLogo extends React.Component {
         <View style={Style.logoContainer}>
           <Image style={Style.logo} source={this.props.source} />
         </View>
-        <View style={Style.loaderContainer}>
-          <LottieView
-            style={Style.loader}
-            source={require('../../animation/src/loader-1.json')}
-            autoPlay
-            resizeMode='cover'
-            autoSize={true}
-            speed={1.5} />
-        </View>
       </View>
     )
   }
