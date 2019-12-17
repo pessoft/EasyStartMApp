76b01907f711ecd6fecbf57ea8ac867f8a0e54fc
apastuhov <apastuhov@mmtr.ru>
2019-12-17 09:19:16 +0300

    Addeed loader indicator after click in iconbutton

--- src/components/buttons/Square/IconButton.js
+++ src/components/buttons/Square/IconButton.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TouchableHighlight } from 'react-native'
+import { TouchableWithoutFeedback, ActivityIndicator, View } from 'react-native'
 import Style from './style'
 import { getSVGColor } from '../../../helpers/color-helper'
 
@@ -7,7 +7,12 @@ export class IconButton extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            showIndicator: false
+        }
+
         this.defaultColor = '#000'
+        this.unmount = false
     }
 
     renderIcon() {
@@ -29,25 +34,35 @@ export class IconButton extends React.Component {
         return {}
     }
 
+    componentWillUnmount() {
+        this.unmount = true
+    }
+
     onPress = () => {
         if (!this.props.disabled && this.props.onPress)
-            this.props.onPress()
+            this.setState({ showIndicator: true },
+                () => setTimeout(() => {
+                    this.props.onPress()
+                    if (!this.unmount) {
+                        this.setState({ showIndicator: false })
+                    }
+                }, 250))
     }
 
     render() {
         return (
-            <TouchableHighlight
-                underlayColor={this.props.underlayColor}
-                style={[
+            <TouchableWithoutFeedback onPress={this.onPress}>
+                <View style={[
                     Style.iconSquareButton,
                     this.isNonBorder(),
                     {
                         borderColor: this.props.borderColor || this.defaultColor,
                         backgroundColor: this.props.backgroundColor
-                    }]}
-                onPress={this.onPress} >
-                {this.renderIcon()}
-            </TouchableHighlight>
+                    }]}>
+                    {!this.state.showIndicator && this.renderIcon()}
+                    {this.state.showIndicator && <ActivityIndicator size={this.props.size} color={this.props.color} />}
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
