9454c55be6b5775241d8802114052949ca31a47f
Alexey Pastukhov <maxell-07@mail.ru>
2020-02-25 23:53:14 +0300

    Added extension for image push notification

---
+++ ios/NotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationServiceExtension
+//
+//  Created by Alexey Pastukhov on 25.02.2020.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
