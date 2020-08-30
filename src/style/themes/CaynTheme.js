43dca78326de6e195a09cb8145f9cdbbf6c4f9a4
apastukhov <sharpmsil@gmail.com>
2020-08-30 14:07:04 +0300

    Added disabled text color in styles

--- src/style/themes/CaynTheme.js
+++ src/style/themes/CaynTheme.js
@@ -46,6 +46,9 @@ export const CaynTheme = StyleSheet.create({
     secondaryTextColor: {
         color: '#757575'
     },
+    disabledTextColor: {
+        color: '#cacaca'
+    },
     dividerColor: {
         borderColor: '#BDBDBD'
     },
