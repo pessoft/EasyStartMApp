4743f0de9683a0ac33ee99e21df0c9d04f7fceee
apastukhov <sharpmsil@gmail.com>
2020-11-15 15:12:57 +0300

    Fixed android push notification

--- src/push-services/push-notification.js
+++ src/push-services/push-notification.js
@@ -1,8 +1,10 @@
 import PushNotification from 'react-native-push-notification'
 import PushNotificationIOS from '@react-native-community/push-notification-ios'
 import { Platform, DeviceEventEmitter } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 let registerPushOnNotificationIOS = {}
+export const STORAGE_PUSH_KEY = '@STORAGE_PUSH_MESSAGES'
 
 export const setupPushNotification = (handleNotification) => {
   PushNotification.configure({
@@ -14,35 +16,44 @@ export const setupPushNotification = (handleNotification) => {
     popInitialNotification: true,
     requestPermissions: true,
     onNotification: async function (notification) {
-      if (Platform.OS == 'android') {
-        let data = notification.data || JSON.parse(notification.payload)
-        if (handleNotification)
-          handleNotification(data)
-      } else if (Platform.OS == 'ios') {
-        const gcmMessageId = notification.data['gcm.message_id']
-        let isTapped = !!registerPushOnNotificationIOS[gcmMessageId]
-
-        if(!isTapped) {
-          let initialNotification = await PushNotificationIOS.getInitialNotification()
-          if(initialNotification) {
-            const initialGcmMessageId = initialNotification._data['gcm.message_id']
-            isTapped = initialGcmMessageId == gcmMessageId
-          }
-        }
-        
-        if (handleNotification && isTapped) {
-          let data = JSON.parse(notification.data.payload)
-          handleNotification(data)
-          
-          delete registerPushOnNotificationIOS[gcmMessageId]
-        } else {
-          registerPushOnNotificationIOS[gcmMessageId] = true
-        }
-
-        notification.finish(PushNotificationIOS.FetchResult.NoData);
-      }
+      await onNotification(notification, handleNotification)
     },
   })
 
   return PushNotification
+}
+
+const onNotification = async (notification, handleNotification) => {
+  if (Platform.OS == 'android') {
+    let data = null
+    if (!notification.foreground)
+      data = notification.data ? JSON.parse(notification.data.payload) : JSON.parse(notification.payload)
+    else if (notification.messageId)
+      data = notification.data || JSON.parse(notification.payload)
+
+    if (handleNotification)
+      handleNotification(data)
+  } else if (Platform.OS == 'ios') {
+    const gcmMessageId = notification.data['gcm.message_id']
+    let isTapped = !!registerPushOnNotificationIOS[gcmMessageId]
+
+    if (!isTapped) {
+      let initialNotification = await PushNotificationIOS.getInitialNotification()
+      if (initialNotification) {
+        const initialGcmMessageId = initialNotification._data['gcm.message_id']
+        isTapped = initialGcmMessageId == gcmMessageId
+      }
+    }
+
+    if (handleNotification && isTapped) {
+      let data = JSON.parse(notification.data.payload)
+      handleNotification(data)
+
+      delete registerPushOnNotificationIOS[gcmMessageId]
+    } else {
+      registerPushOnNotificationIOS[gcmMessageId] = true
+    }
+
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  }
 }
