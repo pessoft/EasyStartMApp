21c21162791983e6c08cb899f065fc09c4f204f7
apastukhov <sharpmsil@gmail.com>
2021-06-19 14:47:14 +0300

    update rn version

--- metro-config.js
+++ metro-config.js
@@ -10,7 +10,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
         babelTransformerPath: require.resolve("react-native-svg-transformer"),
       },
       resolver: {
