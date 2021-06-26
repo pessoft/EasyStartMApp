8d6760564908e99f3faef667cc0dd82d4b5fcc7f
pess <sharpmsil@gmail.com>
2021-06-26 18:26:34 +0300

    Добавлены килограммы и штуки информация о товаре

--- src/helpers/product-additional-option.js
+++ src/helpers/product-additional-option.js
@@ -2,12 +2,16 @@ export const ProductAdditionalInfoType = {
     Custom: 0,
     Grams: 1,
     Milliliters: 2,
-    Liters: 3
+    Liters: 3,
+    Kilograms: 4,
+    Pieces: 5
 }
 
 export const ProductAdditionalInfoTypeShortName = {
     0: '',
     1: 'гр.',
     2: 'мл',
-    3: 'л'
+    3: 'л',
+    4: 'кг',
+    5: 'шт.'
 }
